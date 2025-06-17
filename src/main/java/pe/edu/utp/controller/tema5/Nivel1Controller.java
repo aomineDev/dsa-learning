6e248e35639ad6f19f5b21beec59f9c6caef4300
aomine <aomine1745@gmail.com>
2025-06-17 16:37:03 -0500

    first comiit

---
+++ src/main/java/pe/edu/utp/controller/tema5/Nivel1Controller.java
@@ -0,0 +1,24 @@
+package pe.edu.utp.controller.tema5;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.web.WebView;
+
+public class Nivel1Controller {
+  @FXML
+  private WebView code1;
+
+  @FXML
+  private WebView code2;
+
+  @FXML
+  void initialize() {
+    code1.getEngine().load(getClass().getResource("/pe/edu/utp/monaco/tema5/t5n1_1.html").toExternalForm());
+    code2.getEngine().load(getClass().getResource("/pe/edu/utp/monaco/tema5/t5n1_2.html").toExternalForm());
+  }
+
+  @FXML
+  void handleContinueBtnClick(ActionEvent event) {
+
+  }
+}
---
+++ src/main/java/pe/edu/utp/controller/tema5/Nivel1Quizz1Controller.java
@@ -0,0 +1,5 @@
+package pe.edu.utp.controller.tema5;
+
+public class Nivel1Quizz1Controller {
+
+}
