ba56edd3e33478cd728cb59f828824c8ca7542d2
Jhordan <calixto4211@gmail.com>
2025-06-17 23:50:44 -0500

    quizz nivel y el nivel 2

---
+++ src/main/java/pe/edu/utp/controller/tema2/Nivel1Quizz1Controller.java
@@ -0,0 +1,5 @@
+package pe.edu.utp.controller.tema2;
+
+public class Nivel1Quizz1Controller {
+
+}
---
+++ src/main/java/pe/edu/utp/controller/tema2/Nivel2Controller.java
@@ -0,0 +1,30 @@
+package pe.edu.utp.controller.tema2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.web.WebView;
+
+public class Nivel2Controller {
+
+    @FXML
+    private WebView code1;
+
+    @FXML
+    private WebView code2;
+
+    @FXML
+    private WebView code3;
+
+    @FXML
+    void initialize() {
+        code1.getEngine().load(getClass().getResource("/pe/edu/utp/monaco/tema2/t2n2_1.html").toExternalForm());
+        code2.getEngine().load(getClass().getResource("/pe/edu/utp/monaco/tema2/t2n2_2.html").toExternalForm());
+        code3.getEngine().load(getClass().getResource("/pe/edu/utp/monaco/tema2/t2n2_3.html").toExternalForm());
+    }
+
+    @FXML
+    void handleaactionbtn(ActionEvent event) {
+
+    }
+
+}
