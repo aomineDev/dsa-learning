5bba7ea5e987a1970f52bced9404e1c709e2b1e1
Jhordan <calixto4211@gmail.com>
2025-06-19 12:32:31 -0500

    nivel 4 y 5 tema1

--- src/main/java/pe/edu/utp/controller/tema1/Nivel5Controller.java
+++ src/main/java/pe/edu/utp/controller/tema1/Nivel5Controller.java
@@ -1,21 +1,45 @@
 package pe.edu.utp.controller.tema1;
 
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.Parent;
-import javafx.stage.Stage;
-import javafx.event.ActionEvent;
-import javafx.scene.Node;
-
 import java.io.IOException;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.web.WebView;
+import pe.edu.utp.App;
+
 public class Nivel5Controller {
+
     @FXML
-    private void irAlQuiz(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/tema1/Nivel5Quizz1View.fxml"));
-        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
-        stage.setScene(new Scene(root));
-        stage.show();
+    private WebView code1;
+
+    @FXML
+    private WebView code2;
+
+    @FXML
+    private ComboBox<String> nivelSelector;
+
+    @FXML
+    void handleNivelSelectorClick(ActionEvent event) throws IOException {
+        String nivel = nivelSelector.getSelectionModel().getSelectedIndex() + 1 + "";
+        App.setRoot("tema1/Nivel" + nivel + "View");
     }
+
+    @FXML
+    void initialize() {
+        code1.getEngine().load(getClass().getResource("/pe/edu/utp/monaco/tema1/t1n5_1.html").toExternalForm());
+        code2.getEngine().load(getClass().getResource("/pe/edu/utp/monaco/tema1/t1n5_2.html").toExternalForm());
+        nivelSelector.getItems().addAll("nivel 1", "nivel 2", "nivel 3", "nivel 4", "nivel 5");
+    }
+
+    @FXML
+    void handleContinueBtnClick(ActionEvent event) throws IOException {
+        App.setRoot("tema1/Nivel5Qizz1View");
+    }
+
+    @FXML
+    void handleGoBackBtnClick(ActionEvent event) throws IOException {
+        App.setRoot("HomeView");
+    }
+
 }
