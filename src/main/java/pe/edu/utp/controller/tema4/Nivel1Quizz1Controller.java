15df9955a523e6b96715180353d7a85b60424fd6
leo <leonardoelbaneado@gmail.com>
2025-06-18 19:09:05 -0500

    logicatema4

--- src/main/java/pe/edu/utp/controller/tema4/Nivel1Quizz1Controller.java
+++ src/main/java/pe/edu/utp/controller/tema4/Nivel1Quizz1Controller.java
@@ -2,6 +2,7 @@ package pe.edu.utp.controller.tema4;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.RadioButton;
 
 public class Nivel1Quizz1Controller {
@@ -22,6 +23,24 @@ public class Nivel1Quizz1Controller {
 
     @FXML
     void guardar(ActionEvent event) {
+
+        boolean correctosSelccionado = this.rdbtnColas.isSelected() && this.rdbtnPilas.isSelected()
+                && this.rdbtnListasEnlazadas.isSelected();
+        boolean incorrectosSeleccionados = !this.rdbtnString.isSelected() && !this.rdbtnArrays.isSelected();
+        if (correctosSelccionado && incorrectosSeleccionados) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Mensaje");
+            alert.setHeaderText(null);
+            alert.setContentText("¡Excelente!");
+            alert.showAndWait();
+    
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Mensaje");
+            alert.setHeaderText(null);
+            alert.setContentText("Error");
+            alert.showAndWait();
+        }
 
     }
 
--- src/main/java/pe/edu/utp/controller/tema4/Nivel2Quizz1Controller.java
+++ src/main/java/pe/edu/utp/controller/tema4/Nivel2Quizz1Controller.java
@@ -2,6 +2,7 @@ package pe.edu.utp.controller.tema4;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.RadioButton;
 
 public class Nivel2Quizz1Controller {
@@ -21,6 +22,22 @@ public class Nivel2Quizz1Controller {
     @FXML
     void confirmar(ActionEvent event) {
 
+        boolean correctosSelccionado = this.rdbtnVGenerarAleatorios.isSelected() && this.rdbtnVCalcularPromedio.isSelected();
+      boolean incorrectosSeleccionados = !this.rdbtnVInsertar.isSelected() && !this.rdbtnVMostrar.isSelected();
+        if (correctosSelccionado && incorrectosSeleccionados) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Mensaje");
+            alert.setHeaderText(null);
+            alert.setContentText("¡Excelente!");
+            alert.showAndWait();
+    
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Mensaje");
+            alert.setHeaderText(null);
+            alert.setContentText("Error");
+            alert.showAndWait();
+        }
     }
 
     @FXML
