554482b259c98c5c62239ebb25f4f4092dd7a5ab
Jhordan <calixto4211@gmail.com>
2025-06-18 21:45:50 -0500

    avance tema 2

--- src/main/java/pe/edu/utp/App.java
+++ src/main/java/pe/edu/utp/App.java
@@ -19,7 +19,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         loadPoppinsFont();
-        scene = new Scene(loadFXML("tema2/Nivel2Quizz1View"));
+        scene = new Scene(loadFXML("tema2/Nivel4View"));
         stage.setScene(scene);
         stage.show();
     }
