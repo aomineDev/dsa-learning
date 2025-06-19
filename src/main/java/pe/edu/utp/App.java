97021ba2b35b950c5cbb404754bdd28009530916
Jhordan <calixto4211@gmail.com>
2025-06-18 23:31:26 -0500

    css terminado tema 2

--- src/main/java/pe/edu/utp/App.java
+++ src/main/java/pe/edu/utp/App.java
@@ -19,7 +19,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         loadPoppinsFont();
-        scene = new Scene(loadFXML("tema2/Nivel4View"));
+        scene = new Scene(loadFXML("tema5/Nivel1Quizz1View"));
         stage.setScene(scene);
         stage.show();
     }
