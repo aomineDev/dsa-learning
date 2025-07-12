83362a885ef827f24b86d53f96cedea45267698f
SaiyukiiL <saiyukiil1029@gmail.com>
2025-07-12 13:05:36 -0500

    tema 13 quizz, monaco y controller

---
+++ src/main/java/pe/edu/utp/controller/tema13/Nivel4Controller.java
@@ -0,0 +1,49 @@
+package pe.edu.utp.controller.tema13;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.web.WebView;
+import pe.edu.utp.App;
+import pe.edu.utp.util.Nivel;
+
+public class Nivel4Controller {
+    @FXML
+    private WebView code1;
+
+
+    @FXML
+    private ComboBox<String> nivelSelector;
+    private String tema;
+    private int nivel;
+
+    @FXML
+    void initialize() {
+        tema = "tema" + 13;
+        nivel = 4;
+
+        Nivel n = new Nivel(tema, nivel);
+
+        n.setNivelItems(nivelSelector, 4);
+        code1.getEngine().load(getClass().getResource("/pe/edu/utp/monaco/tema13/t13n4_1.html").toExternalForm());
+    }
+
+    @FXML
+    void handleGoBackBtnClick(ActionEvent event) throws Exception {
+    App.setRoot("HomeView");
+  }
+
+    @FXML
+    void handleContinueBtnClick(ActionEvent event) throws IOException {
+    App.setRoot("tema13/Nivel4Quizz1View");
+  }
+
+    @FXML
+    void handleNivelSelectorClick(ActionEvent event) throws IOException {
+        String nivel = nivelSelector.getSelectionModel().getSelectedIndex() + 1 + "";
+        App.setRoot(tema + "/Nivel" + nivel + "View");
+    }
+
+}
