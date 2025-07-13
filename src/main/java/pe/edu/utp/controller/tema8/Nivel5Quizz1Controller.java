684ef7255adf9de9b08c04f8f6003be46f15b4f2
Jhordan <calixto4211@gmail.com>
2025-07-12 21:06:46 -0500

    tema 8 terminado

---
+++ src/main/java/pe/edu/utp/controller/tema8/Nivel5Quizz1Controller.java
@@ -0,0 +1,54 @@
+package pe.edu.utp.controller.tema8;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.VBox;
+import pe.edu.utp.App;
+import pe.edu.utp.util.Quizz;
+
+public class Nivel5Quizz1Controller {
+
+    private String[] answers = { "siguiente", "nuevo", "dato", "siguiente" };
+
+    @FXML
+    private VBox btnContainer;
+
+    @FXML
+    private VBox code;
+
+    @FXML
+    private Button nextBtn;
+
+    @FXML
+    void handleConfirmBtnClick(ActionEvent event) {
+        boolean correct = Quizz.handleQuizzInputs(code, answers);
+
+        if (correct) {
+            btnContainer.getChildren().removeFirst();
+            nextBtn.setVisible(true);
+        }
+    }
+
+    @FXML
+    void handleNextBtnClick(ActionEvent event) throws IOException {
+        App.setRoot("HomeView");
+    }
+
+    @FXML
+    void handleGoBackBtnClick(ActionEvent event) throws IOException {
+        App.setRoot("HomeView");
+    }
+
+    @FXML
+    void handleInputTyped(KeyEvent event) {
+        TextField textField = (TextField) event.getSource();
+        textField.getStyleClass().remove("quizz-code--error");
+        textField.getStyleClass().remove("quizz-code--success");
+    }
+
+}
