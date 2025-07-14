8efb5973092e4aca6027c88f25100e522c517345
aomine <aomine1745@gmail.com>
2025-07-13 22:21:04 -0500

    codeground controller

--- src/main/java/pe/edu/utp/controller/template/CodegroundController.java
+++ src/main/java/pe/edu/utp/controller/template/CodegroundController.java
@@ -1,13 +1,8 @@
 package pe.edu.utp.controller.template;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.lang.reflect.Method;
 
-import org.codehaus.janino.SimpleCompiler;
-
-import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -17,12 +12,14 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import javafx.scene.web.WebView;
-import javafx.util.Duration;
-import netscape.javascript.JSObject;
 import pe.edu.utp.App;
 import pe.edu.utp.util.Codeground;
 
 public class CodegroundController {
+  private Codeground codeground;
+  private Object[] testCase1 = { 0, 0 };
+  private Object[] testCase2 = { 0, 0 };
+
   @FXML
   private Button btnCase1;
 
@@ -61,113 +58,41 @@ public class CodegroundController {
 
   @FXML
   void initialize() {
-
     int tema = 0;
-    // code.getEngine()
-    // .load(getClass().getResource("/pe/edu/utp/monaco/tema" + tema +
-    // "/codeground.html").toExternalForm());
+    codeground = new Codeground(Integer.class);
+
     code.getEngine()
-        .load(getClass().getResource("/pe/edu/utp/monaco/template/codeground.html").toExternalForm());
+        .load(getClass().getResource("/pe/edu/utp/monaco/tema" + tema + "/codeground.html").toExternalForm());
   }
 
   @FXML
-  void handleCloseModalClick(MouseEvent event) {
-    Codeground.closeModal(modal);
+  void handleContinueBtnClick(ActionEvent event) throws IOException {
+    App.setRoot("");
   }
 
   @FXML
   void handleExceuteClick(ActionEvent event) {
-    int errorCount = 0;
-
-    JSObject jsWindow = (JSObject) code.getEngine().executeScript("window");
-    String code = (String) jsWindow.call("getCode");
-
-    ByteArrayOutputStream outputCatched = new ByteArrayOutputStream();
-    PrintStream original = System.out;
-    System.setOut(new PrintStream(outputCatched));
-
-    SimpleCompiler compiler = new SimpleCompiler();
+    PrintStream originalOut = System.out;
 
     try {
-      compiler.cook(code);
+      codeground.compile(code);
 
-      Class<?> clazz = compiler.getClassLoader().loadClass("Ejercicio");
+      codeground.testCase(testCase1[0], testCase1[1]);
+      codeground.renderResult(resultCase1, outputText1, btnCase1);
 
-      Method method = clazz.getMethod("solucion", Integer.class);
-      Object objResult1 = method.invoke(null, 5);
-      resultCase1.setText(objResult1.toString());
+      codeground.testCase(testCase2[0], testCase2[1]);
+      codeground.renderResult(resultCase2, outputText2, btnCase2);
 
-      if (objResult1 instanceof Integer) {
-        if ((Integer) objResult1 == 6) {
-          btnCase1.getStyleClass().remove("test-btn--error");
-
-          if (!btnCase1.getStyleClass().contains("test-btn--success"))
-            btnCase1.getStyleClass().add("test-btn--success");
-        } else {
-          btnCase1.getStyleClass().remove("test-btn--success");
-
-          if (!btnCase1.getStyleClass().contains("test-btn--error"))
-            btnCase1.getStyleClass().add("test-btn--error");
-
-          errorCount++;
-        }
-      }
-
-      String out = outputCatched.toString().trim();
-      outputText1.getStyleClass().remove("console-output__content--error");
-      outputText1.setText(out);
-
-      ByteArrayOutputStream outputCatched2 = new ByteArrayOutputStream();
-      System.setOut(new PrintStream(outputCatched2));
-
-      Object objResult2 = method.invoke(null, 8);
-      resultCase2.setText(objResult2.toString());
-
-      if (objResult2 instanceof Integer) {
-        if ((Integer) objResult2 == 9) {
-          btnCase2.getStyleClass().remove("test-btn--error");
-
-          if (!btnCase2.getStyleClass().contains("test-btn--success"))
-            btnCase2.getStyleClass().add("test-btn--success");
-        } else {
-          btnCase2.getStyleClass().remove("test-btn--success");
-
-          if (!btnCase2.getStyleClass().contains("test-btn--error"))
-            btnCase2.getStyleClass().add("test-btn--error");
-
-          errorCount++;
-        }
-      }
-
-      String out2 = outputCatched2.toString().trim();
-      outputText2.getStyleClass().remove("console-output__content--error");
-      outputText2.setText(out2);
-
-      if (errorCount == 0)
+      if (codeground.isValid())
         Codeground.openModal(modal);
 
     } catch (Exception e) {
-      resultCase1.setText("--");
-      resultCase2.setText("--");
-
-      if (!btnCase1.getStyleClass().contains("test-btn--error"))
-        btnCase1.getStyleClass().add("test-btn--error");
-
-      if (!btnCase2.getStyleClass().contains("test-btn--error"))
-        btnCase2.getStyleClass().add("test-btn--error");
-
-      if (!outputText1.getStyleClass().contains("console-output__content--error"))
-        outputText1.getStyleClass().add("console-output__content--error");
-
-      outputText1.setText("Error:\n" + e.getMessage());
-
-      if (!outputText2.getStyleClass().contains("console-output__content--error"))
-        outputText2.getStyleClass().add("console-output__content--error");
-
-      outputText2.setText("Error:\n" + e.getMessage());
+      codeground.handleCodeError(e);
+      codeground.renderResult(resultCase1, outputText1, btnCase1);
+      codeground.renderResult(resultCase2, outputText2, btnCase2);
     }
 
-    System.setOut(original);
+    System.setOut(originalOut);
   }
 
   @FXML
@@ -187,7 +112,7 @@ public class CodegroundController {
   }
 
   @FXML
-  void handleContinueBtnClick(ActionEvent event) throws IOException {
-    App.setRoot("");
+  void handleCloseModalClick(MouseEvent event) {
+    Codeground.closeModal(modal);
   }
 }
--- src/main/java/pe/edu/utp/util/Codeground.java
+++ src/main/java/pe/edu/utp/util/Codeground.java
@@ -1,14 +1,104 @@
 package pe.edu.utp.util;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.Method;
+
+import org.codehaus.janino.SimpleCompiler;
+
 import javafx.animation.FadeTransition;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.scene.web.WebView;
 import javafx.util.Duration;
+import netscape.javascript.JSObject;
 
 public class Codeground {
-  public static void renderCode(WebView code, String tema) {
-    code.getEngine()
-        .load(code.getClass().getResource("/pe/edu/utp/monaco/tema" + tema + "/codeground.html").toExternalForm());
+  private Method method;
+  private int errorCount;
+  private Class<?> clazz;
+  private String testCaseOutput;
+  private boolean testCaseResult;
+  private String testCaseSystemOutput;
+  private boolean hasError;
+
+  public Codeground(Class<?> clazz) {
+    this.clazz = clazz;
+  }
+
+  public void compile(WebView codeView) throws Exception {
+    errorCount = 0;
+    hasError = false;
+
+    JSObject jsWindow = (JSObject) codeView.getEngine().executeScript("window");
+
+    SimpleCompiler compiler = new SimpleCompiler();
+
+    String code = (String) jsWindow.call("getCode");
+    compiler.cook(code);
+
+    Class<?> ejercicio = compiler.getClassLoader().loadClass("Ejercicio");
+
+    method = ejercicio.getMethod("solucion", clazz);
+  }
+
+  public void testCase(Object param, Object expected) throws Exception {
+    ByteArrayOutputStream outputCatched = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(outputCatched));
+
+    Object objResult = method.invoke(null, param);
+
+    testCaseOutput = objResult.toString();
+
+    testCaseResult = objResult.equals(expected);
+
+    testCaseSystemOutput = outputCatched.toString().trim();
+
+    if (!testCaseResult)
+      errorCount++;
+  }
+
+  public void renderResult(Text testCaseOutput, Text testCaseSystemOutput, Button testCaseBtn) {
+    testCaseOutput.setText(this.testCaseOutput);
+
+    toggleSystemOutputClass(testCaseSystemOutput, !hasError);
+    testCaseSystemOutput.setText(this.testCaseSystemOutput);
+
+    toggleTestCaseBtnClass(testCaseBtn, testCaseResult);
+  }
+
+  public void handleCodeError(Exception e) {
+    testCaseOutput = "--";
+    testCaseResult = false;
+    testCaseSystemOutput = "Error:\n" + e.getMessage();
+    hasError = true;
+  }
+
+  private void toggleSystemOutputClass(Text testCaseSystemOutput, boolean value) {
+    if (value == true) {
+      testCaseSystemOutput.getStyleClass().remove("console-output__content--error");
+      safeAddClass(testCaseSystemOutput, "console-output__content--success");
+    } else {
+      testCaseSystemOutput.getStyleClass().remove("console-output__content--success");
+      safeAddClass(testCaseSystemOutput, "console-output__content--error");
+    }
+  }
+
+  private void toggleTestCaseBtnClass(Button testCaseBtn, boolean value) {
+    if (value == true) {
+      testCaseBtn.getStyleClass().remove("test-btn--error");
+      safeAddClass(testCaseBtn, "test-btn--success");
+    } else {
+      testCaseBtn.getStyleClass().remove("test-btn--success");
+      safeAddClass(testCaseBtn, "test-btn--error");
+    }
+  }
+
+  void safeAddClass(Node node, String className) {
+    if (!node.getStyleClass().contains(className))
+      node.getStyleClass().add(className);
   }
 
   public static void closeModal(StackPane modal) {
@@ -25,5 +115,9 @@ public class Codeground {
     fadeIn.setFromValue(0);
     fadeIn.setToValue(1);
     fadeIn.play();
+  }
+
+  public boolean isValid() {
+    return errorCount == 0;
   }
 }
