675c12e7072932c41348dc85d34457592301a4ce
Jhordan <calixto4211@gmail.com>
2025-07-15 01:42:50 -0500

    refactorizacion tema 3 y 11

--- src/main/java/pe/edu/utp/controller/tema3/Nivel7Quizz1Controller.java
+++ src/main/java/pe/edu/utp/controller/tema3/Nivel7Quizz1Controller.java
@@ -5,54 +5,39 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import pe.edu.utp.App;
+import pe.edu.utp.util.Quizz;
 
 public class Nivel7Quizz1Controller {
 
     @FXML
-    private Button btn1;
-
-    @FXML
-    private Button btn2;
-
-    @FXML
-    private Button btn3;
+    private Button correct;
 
     @FXML
     private Button nextBtn;
 
     @FXML
-    void handleGoBackBtnClick(ActionEvent event) throws Exception {
+    private VBox btnContainer;
+
+    @FXML
+    void initialize() {
+
+    }
+
+    @FXML
+    void handleGoBackBtnClick(ActionEvent event) throws IOException {
         App.setRoot("HomeView");
     }
 
     @FXML
     void handleNextBtnClick(ActionEvent event) throws IOException {
-        App.setRoot("HomeView");
+        App.setRoot("tema3/");
     }
 
     @FXML
     void handleOptionBtnClick(ActionEvent event) {
-        clearAllBtns();
-        Button btn = (Button) event.getSource();
-
-        if (btn.getId().equals("btn2")) {
-            disableAllBtns();
-            btn.getStyleClass().add("btn-quizz--success");
-            nextBtn.setVisible(true);
-        } else {
-            btn.getStyleClass().add("btn-quizz--error");
-        }
-    }
-
-    void clearAllBtns() {
-        btn1.getStyleClass().remove("btn-quizz--error");
-        btn3.getStyleClass().remove("btn-quizz--error");
-    }
-
-    void disableAllBtns() {
-        btn1.setDisable(true);
-        btn3.setDisable(true);
+        Quizz.handleQUizz1Buttons(event, btnContainer, nextBtn);
     }
 
 }
