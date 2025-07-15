4eea8e3a4865b30d667a4ef9840f37f055f37d47
leo <leonardoelbaneado@gmail.com>
2025-07-15 00:45:03 -0500

    x

--- src/main/java/pe/edu/utp/controller/tema6/Nivel3Quizz1Controller.java
+++ src/main/java/pe/edu/utp/controller/tema6/Nivel3Quizz1Controller.java
@@ -2,77 +2,41 @@ package pe.edu.utp.controller.tema6;
 
 import java.io.IOException;
 
-import javafx.animation.PauseTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import pe.edu.utp.App;
+import pe.edu.utp.util.Quizz;
 
 public class Nivel3Quizz1Controller {
 
     @FXML
-    private Button Regresar;
+    private Button correct;
 
     @FXML
-    private Button boton1;
+    private Button nextBtn;
 
     @FXML
-    private Button boton2;
+    private VBox btnContainer;
 
     @FXML
-    private Button boton3;
+    void initialize() {
 
-    @FXML
-    private Button boton4;
-
-    @FXML
-    void btnRegresar(ActionEvent event) throws IOException {
-    App.setRoot("HomeView");
     }
 
     @FXML
-    void hundleActionboton1(ActionEvent event) {
-        boton1.getStyleClass().add("btn-quizz--error");
-        boton2.getStyleClass().remove("btn-quizz--error");
-        boton3.getStyleClass().remove("btn-quizz--success");
-        boton4.getStyleClass().remove("btn-quizz--error");
+    void handleGoBackBtnClick(ActionEvent event) throws IOException {
+        App.setRoot("HomeView");
     }
 
     @FXML
-    void hundleActionboton2(ActionEvent event) {
-        boton1.getStyleClass().remove("btn-quizz--error");
-        boton2.getStyleClass().add("btn-quizz--error");
-        boton3.getStyleClass().remove("btn-quizz--success");
-        boton4.getStyleClass().remove("btn-quizz--error");
+    void handleNextBtnClick(ActionEvent event) throws IOException {
+        App.setRoot("tema6/Nivel4CodegroundView");
     }
 
     @FXML
-    void hundleActionboton3(ActionEvent event) {
-        boton1.getStyleClass().remove("btn-quizz--error");
-        boton2.getStyleClass().remove("btn-quizz--error");
-        boton3.getStyleClass().add("btn-quizz--success");
-        boton4.getStyleClass().remove("btn-quizz--error");
-        PauseTransition delay = new PauseTransition(javafx.util.Duration.seconds(3));
-        delay.setOnFinished(e -> {
-            try {
-             App.setRoot("HomeView");  
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        });
-        delay.play();
-    }
-
-    @FXML
-    void hundleActionboton4(ActionEvent event) {
-        boton1.getStyleClass().remove("btn-quizz--error");
-        boton2.getStyleClass().remove("btn-quizz--error");
-        boton3.getStyleClass().remove("btn-quizz--success");
-        boton4.getStyleClass().add("btn-quizz--error");
-    }
-
-
-    public void initialize() {
-        // Hint: initialize() will be called when the associated FXML has been completely loaded.
+    void handleOptionBtnClick(ActionEvent event) {
+        Quizz.handleQUizz1Buttons(event, btnContainer, nextBtn);
     }
 }
