7a9be6ac49a70095be0a290f4cea6c0f1125a4d6
leo <leonardoelbaneado@gmail.com>
2025-07-14 22:13:02 -0500

    puzzle tema 7

--- src/main/java/pe/edu/utp/controller/tema4/Nivel1Quizz1Controller.java
+++ src/main/java/pe/edu/utp/controller/tema4/Nivel1Quizz1Controller.java
@@ -1,101 +1,41 @@
 package pe.edu.utp.controller.tema4;
 
-import javafx.util.Duration;
-import javafx.animation.PauseTransition;
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
-import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
 import pe.edu.utp.App;
+import pe.edu.utp.util.Quizz;
 
 public class Nivel1Quizz1Controller {
     @FXML
-    private RadioButton rdbtnArrays;
+    private Button correct;
 
     @FXML
-    private RadioButton rdbtnColas;
+    private Button nextBtn;
 
     @FXML
-    private RadioButton rdbtnListasEnlazadas;
+    private VBox btnContainer;
 
     @FXML
-    private RadioButton rdbtnPilas;
+    void initialize() {
 
-    @FXML
-    private RadioButton rdbtnString;
-
-    @FXML
-    private Button btnconfirmar;
-
-    @FXML
-    public void initialize() {
-        agregarListenersCambio();
-    }
-
-    private void agregarListenersCambio() {
-        rdbtnColas.setOnAction(e -> resetearEstiloBoton());
-        rdbtnPilas.setOnAction(e -> resetearEstiloBoton());
-        rdbtnListasEnlazadas.setOnAction(e -> resetearEstiloBoton());
-        rdbtnArrays.setOnAction(e -> resetearEstiloBoton());
-        rdbtnString.setOnAction(e -> resetearEstiloBoton());
-    }
-
-    private void resetearEstiloBoton() {
-        if (!btnconfirmar.getStyleClass().contains("btn-next")) {
-            btnconfirmar.getStyleClass().removeAll("btn-quizz--error", "btn-quizz--success");
-            btnconfirmar.getStyleClass().add("btn-next");
-        }
     }
 
     @FXML
-    void guardar(ActionEvent event) {
-
-        boolean correctosSeleccionados = this.rdbtnColas.isSelected()
-                && this.rdbtnPilas.isSelected()
-                && this.rdbtnListasEnlazadas.isSelected();
-
-        boolean incorrectosSeleccionados = !this.rdbtnString.isSelected()
-                && !this.rdbtnArrays.isSelected();
-
-        if (correctosSeleccionados && incorrectosSeleccionados) {
-
-            this.btnconfirmar.getStyleClass().remove("btn-next");
-            this.btnconfirmar.getStyleClass().addAll("btn-quizz--success");
-
-            PauseTransition delay = new PauseTransition(Duration.seconds(3));
-            delay.setOnFinished(e -> {
-                try {
-                    App.setRoot("tema4/Nivel2View");
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            });
-            delay.play();
-
-        } else {
-            this.btnconfirmar.getStyleClass().remove("btn-next");
-            this.btnconfirmar.getStyleClass().addAll("btn-quizz--error");
-        }
-
-        this.rdbtnArrays.setSelected(false);
-        this.rdbtnColas.setSelected(false);
-        this.rdbtnListasEnlazadas.setSelected(false);
-        this.rdbtnPilas.setSelected(false);
-        this.rdbtnString.setSelected(false);
-    }
-
-    @FXML
-    void retroceder(ActionEvent event) throws Exception {
+    void handleGoBackBtnClick(ActionEvent event) throws IOException {
         App.setRoot("HomeView");
     }
 
     @FXML
-    void avanzar(ActionEvent event) {
-        // Por si luego usas este botón
+    void handleNextBtnClick(ActionEvent event) throws IOException {
+        App.setRoot("tema4/Nivel2View");
     }
 
+    @FXML
+    void handleOptionBtnClick(ActionEvent event) {
+        Quizz.handleQUizz1Buttons(event, btnContainer, nextBtn);
+    }
 }
--- src/main/java/pe/edu/utp/controller/tema4/Nivel2Quizz1Controller.java
+++ src/main/java/pe/edu/utp/controller/tema4/Nivel2Quizz1Controller.java
@@ -1,82 +1,42 @@
 package pe.edu.utp.controller.tema4;
 
-import javafx.animation.PauseTransition;
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
-import javafx.util.Duration;
+import javafx.scene.layout.VBox;
 import pe.edu.utp.App;
+import pe.edu.utp.util.Quizz;
 
 public class Nivel2Quizz1Controller {
 
     @FXML
-    private Button btnconfirmar;
+    private Button correct;
 
     @FXML
-    private RadioButton rdbtnVCalcularPromedio;
+    private Button nextBtn;
 
     @FXML
-    private RadioButton rdbtnVGenerarAleatorios;
+    private VBox btnContainer;
 
     @FXML
-    private RadioButton rdbtnVInsertar;
+    void initialize() {
 
-    @FXML
-    private RadioButton rdbtnVMostrar;
-
-    @FXML
-    public void initialize() {
-        agregarListenersCambio();
-    }
-
-    private void agregarListenersCambio() {
-        rdbtnVCalcularPromedio.setOnAction(e -> resetearEstiloBoton());
-        rdbtnVGenerarAleatorios.setOnAction(e -> resetearEstiloBoton());
-        rdbtnVInsertar.setOnAction(e -> resetearEstiloBoton());
-        rdbtnVMostrar.setOnAction(e -> resetearEstiloBoton());
-    }
-
-    private void resetearEstiloBoton() {
-        if (!btnconfirmar.getStyleClass().contains("btn-next")) {
-            btnconfirmar.getStyleClass().removeAll("btn-quizz--error", "btn-quizz--success");
-            btnconfirmar.getStyleClass().add("btn-next");
-        }
     }
 
     @FXML
-    void confirmar(ActionEvent event) {
-        boolean correctosSelccionado = this.rdbtnVGenerarAleatorios.isSelected()
-                && this.rdbtnVCalcularPromedio.isSelected();
-        boolean incorrectosSeleccionados = !this.rdbtnVInsertar.isSelected()
-                && !this.rdbtnVMostrar.isSelected();
-
-        if (correctosSelccionado && incorrectosSeleccionados) {
-            this.btnconfirmar.getStyleClass().remove("btn-next");
-            this.btnconfirmar.getStyleClass().addAll("btn-quizz--success");
-
-            PauseTransition delay = new PauseTransition(Duration.seconds(3));
-            delay.setOnFinished(e -> {
-                try {
-                    App.setRoot("tema4/PuzzleTema4");
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            });
-            delay.play();
-        } else {
-            this.btnconfirmar.getStyleClass().remove("btn-next");
-            this.btnconfirmar.getStyleClass().addAll("btn-quizz--error");
-        }
-
-        this.rdbtnVCalcularPromedio.setSelected(false);
-        this.rdbtnVGenerarAleatorios.setSelected(false);
-        this.rdbtnVInsertar.setSelected(false);
-        this.rdbtnVMostrar.setSelected(false);
-    }
-
-    @FXML
-    void retroceder(ActionEvent event) throws Exception {
+    void handleGoBackBtnClick(ActionEvent event) throws IOException {
         App.setRoot("HomeView");
+    }
+
+    @FXML
+    void handleNextBtnClick(ActionEvent event) throws IOException {
+        App.setRoot("tema4/PuzzleTema4");
+    }
+
+    @FXML
+    void handleOptionBtnClick(ActionEvent event) {
+        Quizz.handleQUizz1Buttons(event, btnContainer, nextBtn);
     }
 }
