67ff88894b48c5a5e07740494c3c80207e424789
leo <leonardoelbaneado@gmail.com>
2025-07-15 04:27:26 -0500

    x

--- src/main/java/pe/edu/utp/controller/tema5/Nivel3Quizz3Controller.java
+++ src/main/java/pe/edu/utp/controller/tema5/Nivel3Quizz3Controller.java
@@ -8,7 +8,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.TextFlow;
 import pe.edu.utp.App;
 import pe.edu.utp.util.Quizz;
 
@@ -25,16 +24,12 @@ public class Nivel3Quizz3Controller {
   private Button nextBtn;
 
   @FXML
-  private TextFlow result;
-
-  @FXML
   void handleConfirmBtnClick(ActionEvent event) {
     boolean correct = Quizz.handleQuizzInputs(code, answers);
 
     if (correct) {
       btnContainer.getChildren().removeFirst();
       nextBtn.setVisible(true);
-      result.setVisible(true);
     }
   }
 
